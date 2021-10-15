cddc888c5d7be6d84a37122ab8425279ac982d23
Prasadkpd <2019cs078@stu.ucsc.lk>
2021-10-15 08:22:44 +0530

    Books create done

--- src/component/ReadingArea/Books.tsx
+++ src/component/ReadingArea/Books.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import {Row} from "react-bootstrap";
 import Header from "../common/Header";
-import {IAuthor, IBook} from "../Types/Types";
+import {IAuthor, IBook, IPopupAlert} from "../Types/Types";
 import NoitemList from "../common/NoitemList";
 import BookList from "../books/BookList";
 import AddItem from "../common/AddItem";
 import BookForm from "../books/BookForm";
+import PopupAlert from "../common/PopUpAlert";
 
 type BooksProps = {
     authors: IAuthor[] | null
@@ -13,10 +14,13 @@ type BooksProps = {
 
 const Books: React.FC<BooksProps> = (props) => {
 
+    const {authors} = props;
     const [books, setBooks] = useState<IBook[] | null>(null);
-    const [isShowBookForm, setIsShowBookForm] = useState<boolean>(true);
+    const [isShowBookForm, setIsShowBookForm] = useState<boolean>(false);
     const [editClicked, setEditClicked] = useState<boolean>(false);
-    
+    const [popupAlert, setPopupAlert] = useState<IPopupAlert | null>(null);
+    const [isShowPopupAlert,setIsShowPopupAlert] = useState<boolean>(false);
+
     const handleAddBookForm = () => {
       setEditClicked(false);
       setIsShowBookForm(true);
@@ -26,14 +30,27 @@ const Books: React.FC<BooksProps> = (props) => {
       setEditClicked(false);
       setIsShowBookForm(false);
     }
+
+    const handleOnCreateBookSubmit = (newBook: IBook) => {
+      const newBookList: IBook[] = books ? books.slice() : [];
+      newBookList.push(newBook);
+      setBooks(newBookList);
+      setPopupAlert({
+          alert: "New Book added Successfully",
+          className:"alert-success"
+      });
+      setIsShowPopupAlert(true);
+    };
     
     const FormRendering = (isShowBookForm: boolean, editClicked:boolean) => {
         if(isShowBookForm){
             if(!editClicked){
-                return <BookForm task="Create" onCloseClick={handleCloseBookForm} editClicked={false}/>
+                return <BookForm task="Create" onCloseClick={handleCloseBookForm} editClicked={false}
+                                 onCreateBookSubmit={handleOnCreateBookSubmit} authorList={authors}/>
             }
             else {
-                return <BookForm task="Update" onCloseClick={handleCloseBookForm} editClicked={true}/>
+                return <BookForm task="Update" onCloseClick={handleCloseBookForm} editClicked={true}
+                                 onCreateBookSubmit={handleOnCreateBookSubmit} authorList={authors}/>
             }
         }
     }
@@ -43,6 +60,7 @@ const Books: React.FC<BooksProps> = (props) => {
             <Header header={"Books"}/>
             {(!books || books.length === 0) && <NoitemList itemtype="Books"/>}
             {(books) && <BookList books={books}/>}
+            <PopupAlert alert={popupAlert} isPopupAlertShow={isShowPopupAlert}/>
             <AddItem item_type={"Book"} onAddClick={handleAddBookForm}/>
             {FormRendering(isShowBookForm,editClicked)}
         </Row>
--- src/component/books/BookForm.tsx
+++ src/component/books/BookForm.tsx
@@ -1,23 +1,39 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, FormEvent, useEffect, useState} from 'react';
 import {Col, Form, FormControl, Row} from "react-bootstrap";
 import {FiXCircle} from "react-icons/fi";
 import FormButton from "../common/FormButton";
-import {IAuthor, selectorOptionType} from "../Types/Types";
+import {IAuthor, IBook, selectorOptionType} from "../Types/Types";
 import CurrencyInput from "react-currency-input-field";
 import Select from "react-select";
+import {CurrencyInputOnChangeValues, CurrencyInputProps}
+    from "react-currency-input-field/dist/components/CurrencyInputProps";
 
 type BookFormProps = {
     task: string,
     onCloseClick: () => void,
-    editClicked: boolean
+    editClicked: boolean,
+    onCreateBookSubmit: (newBook: IBook) => void,
+    authorList: IAuthor[] | null;
 }
 
 const BookForm: React.FC<BookFormProps> = (props) => {
 
-    const {task, onCloseClick, editClicked} = props;
-    const prefix = "Rs";
-    const [optionList, setOptionList] = useState<selectorOptionType[] | null>(null);
+    const {task, onCloseClick, editClicked, onCreateBookSubmit, authorList} = props;
+    const [title, setTitle] = useState<string | null>(null);
+    const [price, setPrice] = useState<string | null>(null);
+    const [author, setAuthor] = useState<IAuthor | null>(null);
+    const [isFormValidate, setIsFormValidate] = useState<boolean>(false);
+    const [isSelectorValidate, setIsSelectorValidate] = useState<boolean>(false);
     const [selectorBorderColor, setSelectorBorderColor] = useState<string>("#959595");
+    const [optionList, setOptionList] = useState<selectorOptionType[] | null>(null);
+
+    const prefix = "Rs";
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [className, setClassName] = useState<string>("");
+    const [value, setValue] = useState<string | number>(0);
+    const [values, setValues] = useState<CurrencyInputOnChangeValues>();
+    const [rawValue, setRawValue] = useState<string | undefined>("");
+
 
     const customStyles = {
       control: (provided: any) => ({
@@ -26,6 +42,78 @@ const BookForm: React.FC<BookFormProps> = (props) => {
           borderRadius: "0px"
       }),
     };
+
+    const handleOnTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+    }
+
+    const handleOnPriceChange: CurrencyInputProps['onValueChange'] = (value, _,
+                                                                      values): void => {
+        setValues(values);
+        setRawValue(value === undefined ? 'undefined' : value || ' ');
+
+        if(!value) {
+            setClassName('');
+            setValue('');
+            return;
+        }
+
+        if (Number.isNaN(Number(value))){
+            setErrorMessage('Please enter a valid number');
+            setClassName('is-invalid');
+            return;
+        }
+
+        setClassName('is-valid');
+        setPrice(value);
+    }
+
+    const handleOnAuthorChange = (option : any) => {
+      if (option) {
+          setAuthor(option.value);
+          if (isSelectorValidate) {
+              setSelectorBorderColor("#6AB867");
+          }
+      } else {
+          setAuthor(null);
+          if (isSelectorValidate) {
+              setSelectorBorderColor("#f80046");
+          }
+      }
+    };
+
+    const handleOnBookSubmit = (e: FormEvent) => {
+      e.preventDefault();
+      author === null ? setSelectorBorderColor("#f80046") : setSelectorBorderColor("#6AB867");
+
+      if (title === null || price === null || title === "" || price === "" || author === null) {
+          setIsFormValidate(true);
+          setIsSelectorValidate(true);
+      } else {
+          const newBook: IBook = {
+              name: title,
+              price: price,
+              author: author
+          };
+          onCreateBookSubmit(newBook);
+          onCloseClick();
+      }
+    }
+
+    useEffect(() => {
+        if (!authorList) {
+            return ;
+        }
+        let options: selectorOptionType[] = [];
+        for (let i = 0; i < authorList.length; i++) {
+            options.push({
+                label: authorList[i].name,
+                value: authorList[i]
+            });
+        }
+        setOptionList(options)
+    }, [authorList]);
+
 
     return (
         <Col xs={12} lg={10} className="form mt-5 px-0 ms-lg-2">
@@ -40,22 +128,25 @@ const BookForm: React.FC<BookFormProps> = (props) => {
                 </Row>
             </Col>
             <Col xs={12} lg={{span: 11, offset: 1}} className="px-0">
-                <Form className="mt-3 col-lg-10 col-md-12">
+                <Form className="mt-3 col-lg-10 col-md-12" onSubmit={handleOnBookSubmit} validated={isFormValidate}
+                      noValidate>
                     <Form.Group>
                         <Form.Label className="mb-1 form-label ms-1">Book Title</Form.Label>
-                        <Form.Control type="text" required className='form-input py-lg-1'/>
+                        <Form.Control type="text" required className='form-input py-lg-1'
+                                      onChange={handleOnTitleChange}/>
                         <FormControl.Feedback type='invalid'>
                             <p className="text-danger fw-bold">Please Enter Book title</p>
                         </FormControl.Feedback>
-                    </Form.Group>
-                    <Form.Group>
+                        </Form.Group>
+                        <Form.Group>
                         <Form.Label className="mb-1 form-label ms-1">Price</Form.Label>
                         <CurrencyInput placeholder="Please enter a number"
                                         prefix={prefix}
                                         step={1}
-                                        className={`form-control form-input`}/>
+                                        className={`form-control form-input`}
+                                        onValueChange={handleOnPriceChange}/>
                         <FormControl.Feedback type='invalid'>
-                            <p className="text-danger fw-bold">Please Enter Book Price</p>
+                            <p className="text-danger fw-bold">{errorMessage}</p>
                         </FormControl.Feedback>
                     </Form.Group>
                     <Form.Group>
@@ -63,7 +154,8 @@ const BookForm: React.FC<BookFormProps> = (props) => {
                         <Select isSearchable
                                 isClearable
                                 options={!optionList ? [] : optionList}
-                                styles={customStyles}/>
+                                styles={customStyles}
+                                onChange={handleOnAuthorChange}/>
                         <FormControl.Feedback type='invalid'>
                             <p className="text-danger fw-bold">Please Select author</p>
                         </FormControl.Feedback>
